a1517ee1937957329c7786e166321a9199f20d31
Leon Do <leondo123@gmail.com>
2017-03-19 19:39:48 -0400

    flatten image in base64 is working. need to work on cfunction controllArray

--- server/misc/test.js
+++ server/misc/test.js
@@ -1,9 +1,20 @@
-var object = [
-{'0': 'cat'},
-{'1': 'dog'},
-{'2': 'mouse'},
-{'3': 'rat'},
-{'4': 'pig'},
-]
+var myData = require('../data/myData.js')
 
-console.log(object[0]['0'])
+
+var data = myData.array
+console.log(data[0])
+
+var shortestDistance = Infinity;
+var index;
+
+for (var i = 0; i < data.length; i++){
+
+    console.log('EUCLIDEAN DISTANCE')
+    console.log(euclideanDistance(data[i].splice(1),flatArray))
+
+    if (shortestDistance > euclideanDistance(data[i].splice(1),flatArray)){
+        shortestDistance = euclideanDistance(data[i].splice(1),flatArray);
+        index = i;
+    }
+
+}
