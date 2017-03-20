1c056527eff0e99037c1cef96539f7a1a3cf66f4
Leon Do <leondo123@gmail.com>
2017-03-20 01:05:13 -0400

    fix base64img.js file line 90ish. the array part

--- server/misc/test.js
+++ server/misc/test.js
@@ -1,20 +1,5 @@
-var myData = require('../data/myData.js')
+var arr = [4,5,7,4,4,5,7,0]
 
+var index = arr.indexOf(Math.min.apply(Math,arr))
 
-var data = myData.array
-console.log(data[0])
-
-var shortestDistance = Infinity;
-var index;
-
-for (var i = 0; i < data.length; i++){
-
-    console.log('EUCLIDEAN DISTANCE')
-    console.log(euclideanDistance(data[i].splice(1),flatArray))
-
-    if (shortestDistance > euclideanDistance(data[i].splice(1),flatArray)){
-        shortestDistance = euclideanDistance(data[i].splice(1),flatArray);
-        index = i;
-    }
-
-}
+console.log(min)
